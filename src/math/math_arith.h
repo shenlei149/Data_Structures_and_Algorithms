99a1bcae9471f22b5927a8abbf7f8f1751de8d29
Leo Shen <shenlei149@outlook.com>
2021-02-01 12:19:33 +0000

    Fix typo

--- src/math/math_arith.h
+++ src/math/math_arith.h
@@ -1,15 +1,15 @@
 #ifndef MATH_ARITH_INCLUDED
 #define MATH_ARITH_INCLUDED
 
-extern long Max(long x, long y);
-extern long Min(long x, long y);
+extern long Arith_Max(long x, long y);
+extern long Arith_Min(long x, long y);
 
 // always  truncate  toward  the left on the number line
 // (x / y) * y + x % y = x
-extern long Div(long x, long y);
-extern long Mod(long x, long y);
+extern long Arith_Div(long x, long y);
+extern long Arith_Mod(long x, long y);
 
-extern long Ceiling(long x, long y);
-extern long Floor(long x, long y);
+extern long Arith_Ceiling(long x, long y);
+extern long Arith_Floor(long x, long y);
 
 #endif
